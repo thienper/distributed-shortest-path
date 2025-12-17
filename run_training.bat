79236f193628596e625f408b844d6d7dffce04f8
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2025-12-18 03:09:25 +0700

    update

--- run_training.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Train GraphSAGE model for 50 epochs
-cd /d "t:\NAM_4\NAM_4_HK1\DU LIEU LON\BT_LON\streaming-event-clustering\distributed-shortest-path"
-call .venv\Scripts\activate.bat
-python fast_train.py --graph large --samples 1000 --epochs 50
-pause
